c53e43a9893eb7b34fc83eb832b21357f8c5db7d
wsh8115385 <314168424@qq.com>
2018-04-09 12:17:56 +0800

    remodify MyServlet.java

--- MyServlet.java
+++ MyServlet.java
@@ -2,10 +2,8 @@ package com.first;
 
 import java.io.*;
 import java.lang.*;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class MyServlet extends HttpServlet{
 	
